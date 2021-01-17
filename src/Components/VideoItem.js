5a3f2db0ef43cea4062d870b7d466442279ec672
Prathamesh Nimbkar <prathamesh_nimbkar@outlook.com>
2021-01-17 17:56:39 +0530

    added alt attr for image

--- src/Components/VideoItem.js
+++ src/Components/VideoItem.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 
 import './VideoItem.css';
@@ -8,9 +9,12 @@ const VideoItem = ({video, onSelectedVideo}) => {
     return (
         <div className="ui list">
             <div className="item video-item" onClick={() => onSelectedVideo(video)}>
-                <img src={video.snippet.thumbnails.default.url} className="ui image"/>
+                <img 
+                    src={video.snippet.thumbnails.default.url} 
+                    className="ui image"
+                    alt={video.id.videoId}/>
                 <div className="content">
-                    <a className="header" >{video.snippet.title}</a>
+                    <a className="header" href="">{video.snippet.title}</a>
                 </div>
             </div>
         </div>
