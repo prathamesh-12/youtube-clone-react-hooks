6f7e7e53a15a4c7a999a8253fbc08ca9e903f6a0
Prathamesh Nimbkar <prathamesh_nimbkar@outlook.com>
2021-01-17 17:50:37 +0530

    added title attr in iframe - VideoDetail component

--- src/Components/VideoDetail.js
+++ src/Components/VideoDetail.js
@@ -13,6 +13,7 @@ const VideoDetail = ({ video }) => {
                 width="560" height="315" 
                 src={`https://www.youtube.com/embed/${video.id.videoId}`}
                 frameBorder="0" 
+                title={video.id.videoId}
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
             <div className="ui segment">
